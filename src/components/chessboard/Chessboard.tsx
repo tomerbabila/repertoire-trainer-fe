18de8a7e77801ddeb76e4ff63532cd9465ab42ed
tomerbabila <tomer.babila@gmail.com>
2025-06-26 14:46:38 +0300

    Create app using vite

---
+++ src/components/chessboard/Chessboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Chessboard() {
+  return (
+    <div className='w-[480px] h-[480px] border border-gray-300 p-4 flex items-center justify-center'>
+      <span className='text-gray-400 text-xl'>Chessboard goes here</span>
+    </div>
+  );
+}
