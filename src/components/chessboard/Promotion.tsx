f8a043e39d8f07cf0a03a414287c87ead3f00d54
tomerbabila <tomer.babila@gmail.com>
2025-07-07 19:16:56 +0300

    Change colors

--- src/components/chessboard/Promotion.tsx
+++ src/components/chessboard/Promotion.tsx
@@ -9,11 +9,11 @@ interface PromotionProps {
 
 export default function Promotion({ color, onSelect }: PromotionProps) {
   return (
-    <div className='absolute z-50 bg-[#1c1c24] border flex flex-row p-2 top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'>
+    <div className='absolute z-50 bg-[var(--promotion-bg)] border flex flex-row p-2 top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2'>
       {(['q', 'r', 'b', 'n'] as const).map((piece) => (
         <button
           key={piece}
-          className='hover:bg-[#7c7c7c] p-1 flex items-center justify-center'
+          className='hover:bg-[var(--promotion-hover)] p-1 flex items-center justify-center'
           onClick={() => onSelect(piece)}
         >
           <img src={pieceMap[color + piece]} alt={piece} className='w-8 h-8' />
